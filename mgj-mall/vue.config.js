a30bd234fce7477e4e253d82ca4bb3269bf8b861
coder-itl <941755602@qq.com>
2021-05-31 15:12:45 +0800

    docs:update .editorconfig

--- mgj-mall/vue.config.js
+++ mgj-mall/vue.config.js
@@ -3,10 +3,9 @@ module.exports = {
   configureWebpack: {
     resolve: {
       alias: {
-        assets: '@/assets',
-        common: '@/components/common',
-        components: '@/components',
-        network: '@/network',
+        assets: '@/assets', // 静态资源路径别名
+        common: '@/components/common', // 共用组件路径别名【TODO: BUG ，应将 common 作为外层】
+        components: '@/components', // 组件别名
       },
     },
   },

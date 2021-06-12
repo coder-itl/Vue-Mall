325f787db8cc4b65d77863b731ac228815d325e9
coder-itl <941755602@qq.com>
2021-06-12 16:44:03 +0800

    feat:start development cart

--- mgj-mall/src/views/cart/Cart.vue
+++ mgj-mall/src/views/cart/Cart.vue
@@ -1,5 +1,35 @@
 <template>
-  <div id="cart">
-    <h1> 购物车 </h1>
+  <div class="cart">
+    <nav-bar class="nav-bar">
+      <div slot="center" class="center">购物车({{count}})</div>
+    </nav-bar>
   </div>
 </template>
+
+<script>
+import NavBar from '../../components/common/navbar/NavBar.vue'
+
+export default {
+  name: 'Cart',
+  components: {
+    NavBar
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+
+NavBar</script>
+
+<style  scoped>
+.nav-bar {
+  background-color: pink;
+}
+.center {
+  font-size: 20px;
+  font-weight: 700;
+  color: #fff;
+}
+</style>
